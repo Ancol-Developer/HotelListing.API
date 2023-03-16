f66cb088002a16c0f5d1109e065b1f0f87493e69
Ancol <trandieu026@gmail.com>
2023-03-16 17:56:06 +0700

    1/2 part4- video 17 phut thu 11

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -1,8 +1,15 @@
-﻿using Serilog;
+﻿using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(option=>
+{
+    option.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,7 +24,7 @@ builder.Services.AddCors(option => {
         .AllowAnyMethod());
 });
 builder.Host.UseSerilog((ctx, lc)=> lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
-//
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
