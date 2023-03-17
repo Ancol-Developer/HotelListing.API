7f22ca1bbb9cea8ec30050821dfdfabd37246382
Ancol <trandieu026@gmail.com>
2023-03-18 00:45:42 +0700

    Added EntityFramework and Created Database

--- HotelListing.API/HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/HotelListing.API/Data/HotelListingDbContext.cs
@@ -10,5 +10,56 @@ namespace HotelListing.API.Data
         }
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country 
+                { 
+                    Id = 1,
+                    Name="Jamaica",
+                    ShortName="JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id= 3,
+                    Name = "Cayman Island",
+                    ShortName="CI"
+                }
+            );
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   ID = 1,
+                   Name="Sandals Resort and Spa",
+                   Address="Negril",
+                   CountryID=1,
+                   Rating=4.5
+               },
+               new Hotel
+               {
+                   ID = 2,
+                   Name = "Comfort Suites",
+                   Address = "George Town",
+                   CountryID =3,
+                   Rating =4.3
+               },
+               new Hotel
+               {
+                   ID = 3,
+                   Name = "Grand Palldium",
+                   Address = "Nassua",
+                   CountryID =2,
+                   Rating =4
+               }
+
+            );
+        }
     }
 }
